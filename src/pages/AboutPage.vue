94d617f533a76d5ce9f7b698cbc4a2fd5ef43110
stech17 <scctech6@gmail.com>
2025-12-11 15:46:18 +0630

    Update aboutImage URL in AboutPage.vue

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -14,7 +14,7 @@ default: `We make cool stuff and help people learn. This section gives a short i
 },
 aboutImage: {
 type: String,
-default: 'https://your-new-image-link-here.com/image.jpg'
+default: 'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D'
 },
 aboutImageAlt: {
 type: String,
