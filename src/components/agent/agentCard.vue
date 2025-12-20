3084129b78be20fb20f846771eaa5fff04278394
h1k4ru-f14m3 <hikaru.hino.flame@gmail.com>
2025-12-20 13:13:47 +0630

    Update agentCard

--- src/components/agent/agentCard.vue
+++ src/components/agent/agentCard.vue
@@ -17,15 +17,18 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="!loading" class="flex flex-col justify-center items-center" v-for="(item, index) in response.data">
+  <div v-if="!loading" class="flex flex-col justify-center items-center m-3" v-for="(item, index) in response.data">
+
     <div class="bg-base-300 w-[10em] h-[10em] rounded-[100em]">
       <img v-if="img.img_data[index]" :src="img.img_data[index]" class="w-full h-full object-cover rounded-[100em]" loading="lazy"></img>
     </div>
-    <p class="text-3xl p-1 md:text-5xl font-black md:p-3">{{ item[1] }}</p>
-    <p class="text-xl md:text-2xl">{{ item[3] }}</p>
-    <p class="text-xl md:text-2xl">{{ item[4] }}</p>
+
+    <p class="font-shan text-3xl p-1 md:text-5xl font-black md:p-3">{{ item[1] }}</p>
+    <p class="font-shan text-xl md:text-2xl">{{ item[3] }}</p>
+    <p class="font-shan text-xl md:text-2xl">{{ item[4] }}</p>
   </div>
-  <div v-else class="flex flex-col justify-center items-center" v-for="n in 2">
+  
+  <div v-else class="flex flex-col justify-center items-center" v-for="n in 3" :key="n">
     <div class="skeleton bg-base-300 w-[10em] h-[10em] rounded-[100em]">
     </div>
     <p class="skeleton skeleton-text text-3xl p-1 md:text-5xl font-black md:p-3">Name</p>
