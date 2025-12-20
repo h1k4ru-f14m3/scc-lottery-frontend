47b09c3ce1872f08a8051bc2fe6a094b2bc11720
h1k4ru-f14m3 <hikaru.hino.flame@gmail.com>
2025-12-20 14:55:02 +0630

    Added debounce on search.

--- src/components/general_components/search.vue
+++ src/components/general_components/search.vue
@@ -14,6 +14,7 @@ const props = defineProps({
 
 const emit = defineEmits(['gotInput'])
 const searchQuery = ref()
+let debounceTimer = null
 
 const styleClasses = computed(() => {
     if (props.joinItem) {
@@ -22,10 +23,18 @@ const styleClasses = computed(() => {
         return "flex flex-row justify-center m-3"
     }
 })
+
+const handleInput = () => {
+    clearTimeout(debounceTimer)
+
+    debounceTimer = setTimeout(() => {
+        emit('gotInput', searchQuery.value)
+    }, 250)
+}
 </script>
 
 <template>
     <div :class="styleClasses">
-        <input class="input" :class="joinItem ? 'join-item' : ''" :placeholder="props.placeholder" v-model="searchQuery" @input="emit('gotInput', searchQuery)"></input>
+        <input class="input" :class="joinItem ? 'join-item' : ''" :placeholder="props.placeholder" v-model="searchQuery" @input="handleInput"></input>
     </div>
 </template>
