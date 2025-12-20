d312ff2bc813da8e5466f0b1c172c6b6f6a55770
h1k4ru-f14m3 <hikaru.hino.flame@gmail.com>
2025-12-20 06:56:24 +0630

    Added editTickets.

---
+++ src/components/admin/modals/tickets/editTickets.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+import api from '@/api'
+import simpleButton from '@/components/general_components/simpleButton.vue'
+import { vAutoAnimate } from '@formkit/auto-animate'
+
+const props = defineProps({
+  code: [Number, String],
+})
+
+const emits = defineEmits(['submitEdit'])
+</script>
+
+<template></template>
