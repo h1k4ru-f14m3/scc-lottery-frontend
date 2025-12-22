646335d29a7bc1be9947f3c6f1a79195bcff9c7d
stech17 <scctech6@gmail.com>
2025-12-22 08:59:31 +0630

    Update HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -79,7 +79,7 @@ const prizeIMGs = ref([
 
  <div class="mt-[3em]"> 
    <CardsPreview :component-to-use="card" route="/" 
-     class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 items-start"></CardsPreview> </div>
+     inherit-classes="flex flex-wrap gap-2 rounded-md p-4 tab-content md:max-w-[75vw] justify-center items-center"></CardsPreview> </div>
      
 </section>
 
