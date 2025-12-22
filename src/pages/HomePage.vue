5deeb6698558ebe48d526b327a5219a2782e5fbb
stech17 <scctech6@gmail.com>
2025-12-22 09:06:03 +0630

    Update layout of CardsPreview component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -79,7 +79,7 @@ const prizeIMGs = ref([
 
  <div class="mt-[3em]"> 
    <CardsPreview :component-to-use="card" route="/" 
-     inherit-classes="flex flex-wrap gap-2 rounded-md p-4 tab-content md:max-w-[75vw] justify-center items-center"></CardsPreview> </div>
+     class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6"></CardsPreview> </div>
      
 </section>
 
