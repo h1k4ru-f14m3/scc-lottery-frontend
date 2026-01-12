7b1a228fa1fdb367c0a995f80ab281068bc2df2f
h1k4ru-f14m3 <hikaru.hino.flame@gmail.com>
2026-01-12 14:00:39 +0630

    Fixed agents list overlapping text.

--- src/components/agent/agentCard.vue
+++ src/components/agent/agentCard.vue
@@ -20,22 +20,23 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="!loading" class="flex flex-col justify-center items-center m-3" v-for="(item, index) in response.data">
+  <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+    <div v-if="!loading" class="flex flex-col justify-start h-full items-center m-3" v-for="(item, index) in response.data">
+      <div class="bg-base-300 w-[10em] h-[10em] rounded-[100em]">
+        <img v-if="img.img_data[index]" :src="img.img_data[index]" class="w-full h-full object-cover rounded-[100em]" loading="lazy"></img>
+      </div>
 
-    <div class="bg-base-300 w-[10em] h-[10em] rounded-[100em]">
-      <img v-if="img.img_data[index]" :src="img.img_data[index]" class="w-full h-full object-cover rounded-[100em]" loading="lazy"></img>
+      <p class="font-shan text-center wrap-break-word w-full wrap text-3xl p-1 md:text-5xl font-black md:p-3">{{ item[1] }}</p>
+      <p class="font-shan text-xl md:text-2xl">{{ item[3] }}</p>
+      <p class="font-shan text-xl md:text-2xl">{{ item[4] }}</p>
     </div>
-
-    <p class="font-shan text-3xl p-1 md:text-5xl font-black md:p-3">{{ item[1] }}</p>
-    <p class="font-shan text-xl md:text-2xl">{{ item[3] }}</p>
-    <p class="font-shan text-xl md:text-2xl">{{ item[4] }}</p>
-  </div>
-  
-  <div v-else class="flex flex-col justify-center items-center" v-for="n in 3" :key="n">
-    <div class="skeleton bg-base-300 w-[10em] h-[10em] rounded-[100em]">
+    
+    <div v-else class="flex flex-col justify-center items-center" v-for="n in 3" :key="n">
+      <div class="skeleton bg-base-300 w-[10em] h-[10em] rounded-[100em]">
+      </div>
+      <p class="skeleton skeleton-text text-3xl p-1 md:text-5xl font-black md:p-3">Name</p>
+      <p class="skeleton skeleton-text text-xl md:text-2xl">Phone Number</p>
+      <p class="skeleton skeleton-text text-xl md:text-2xl">Address</p>
     </div>
-    <p class="skeleton skeleton-text text-3xl p-1 md:text-5xl font-black md:p-3">Name</p>
-    <p class="skeleton skeleton-text text-xl md:text-2xl">Phone Number</p>
-    <p class="skeleton skeleton-text text-xl md:text-2xl">Address</p>
   </div>
 </template>
